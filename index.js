f264a2bfb66cd9d8f2db030a1c579a647178406d
Adriano França <adrianojfn@gmail.com>
2024-01-08 23:50:35 -0300

    Created the database using PostgreSQL

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.status(200).send('My backend!')
-})
-
 app.listen(3000, () => {
     console.log('Executing backend...')
 })
